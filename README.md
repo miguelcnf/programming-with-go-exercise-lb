cdcf1e3df610cde2399557b4fc834ea86ca1a308
Miguel Fonseca <miguel.cnf@gmail.com>
2019-09-01 21:52:43 +0100

    Update godoc

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Implement the `ProxyFor` method as described in the `internal/proxy/proxy.go` file:
 
 ```go
+// Engine represents the proxy engine.
+type Engine struct {
+	// TODO
+}
+
 // ProxyFor serves the request via the backends configured for the received host.
 // In case host is unknown or has no backends configured it serves a 404 response.
 func (p *Engine) ProxyFor(host string, w http.ResponseWriter, r *http.Request) {
--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -4,12 +4,13 @@ import (
 	"net/http"
 )
 
+// Engine represents the proxy engine.
 type Engine struct {
 	// TODO
 }
 
 // ProxyFor serves the request via the backends configured for the received host.
-// In case host is unknown of has no backends configured it serves a 404 response.
+// In case host is unknown or has no backends configured it serves a 404 response.
 func (p *Engine) ProxyFor(host string, w http.ResponseWriter, r *http.Request) {
 	// TODO
 }
